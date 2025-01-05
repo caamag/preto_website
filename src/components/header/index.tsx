287d62d5543d8618832d22db3dcc89f8723e985a
caio <caio.lopes@bcrcx.com>
2025-01-05 16:35:09 -0300

    Finalizando navegação do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,59 @@
+import * as Css from "./style";
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import sideMenu from "../../assets/side-menu.png";
+
 const Header = () => {
-  return <>teste</>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const sideMenuController = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
+
+  return (
+    <Css.Header>
+      <Css.HeaderContent>
+        <h1>Preto</h1>
+
+        <Css.Navigation>
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/estudio"}>Estúdio</NavLink>
+          <NavLink to={"/casamento"}>Casamento</NavLink>
+          <NavLink to={"/brand"}>Brand</NavLink>
+          <NavLink to={"/propaganda"}>Propaganda</NavLink>
+          <NavLink to={"/curso"}>Curso</NavLink>
+        </Css.Navigation>
+
+        <button
+          onClick={() => {
+            sideMenuController();
+          }}
+        >
+          <img src={sideMenu} alt="Menu lateral" />
+        </button>
+      </Css.HeaderContent>
+
+      <Css.MobileNavigation isOpen={isOpen}>
+        <NavLink to={"/"}>Home</NavLink>
+        <br />
+        <NavLink to={"/estudio"}>Estúdio</NavLink>
+        <br />
+        <NavLink to={"/casamento"}>Casamento</NavLink>
+        <br />
+        <NavLink to={"/brand"}>Brand</NavLink>
+        <br />
+        <NavLink to={"/propaganda"}>Propaganda</NavLink>
+        <br />
+        <NavLink to={"/curso"}>Curso</NavLink>
+      </Css.MobileNavigation>
+    </Css.Header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/style.ts
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  height: 100px;
+  background-color: rgb(0, 0, 0, 0.9);
+  position: fixed;
+
+  .active {
+    color: white;
+  }
+
+  a {
+    color: gray;
+    text-decoration: none;
+    font-size: 1.6rem;
+    font-weight: 500;
+    padding: 8px 15px;
+    border-radius: 5px;
+    transition: 150ms;
+  }
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  h1 {
+    color: white;
+    font-size: 4rem;
+  }
+
+  img {
+    width: 30px;
+    height: 30px;
+    filter: invert(100%);
+    display: none;
+  }
+
+  @media screen and (max-width: 800px) {
+    justify-content: space-between;
+
+    h1 {
+      margin-left: 15px;
+    }
+
+    img {
+      display: block;
+      margin-right: 15px;
+    }
+  }
+`;
+
+export const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (max-width: 800px) {
+    display: none;
+  }
+`;
+
+export const MobileNavigation = styled.div<{ isOpen: boolean }>`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  padding-top: 100px;
+  background-color: rgb(0, 0, 0, 0.8);
+  right: ${(p) => (p.isOpen ? "0px" : "-100%")};
+  transition: right 250ms ease-in-out;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    font-size: 2.5rem;
+  }
+`;
