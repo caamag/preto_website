dcb4a2d21ce925a00996aaef78b7d2cf2c951101
caio <caio.lopes@bcrcx.com>
2025-01-13 19:37:24 -0300

    Ajustando grid de produtos

--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -6,26 +6,35 @@ export const HomeContainer = styled.div``;
 export const SectionsContainer = styled.div`
   width: 100%;
   background-color: rgb(50, 50, 50);
-  padding: 40px 0px;
-`;
-
-export const SectionContent = styled.div`
-  width: 100%;
-  max-width: 1400px;
-  margin: 0 auto;
-
+  padding: 40px 0;
   display: flex;
   justify-content: center;
-  flex-wrap: wrap;
 
   a {
     text-decoration: none;
   }
 `;
 
+export const SectionContent = styled.div`
+  width: 100%;
+  max-width: 2400px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+  padding: 0 16px;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
 export const SectionItem = styled.div<{ bgImagePath: string }>`
-  width: 300px;
-  height: 300px;
+  width: 100%;
+  aspect-ratio: 1;
   background-image: url(${(p) => p.bgImagePath});
   background-position: center;
   background-repeat: no-repeat;
