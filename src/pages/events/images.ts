971db84150dbad714ed3a48530c49713087f032e
caio <caio.lopes@bcrcx.com>
2025-02-03 21:14:03 -0300

    Atualizando import de imagens para otimização

---
+++ src/pages/events/images.ts
@@ -0,0 +1,19 @@
+import eventImage from "./assets/event-banner.jpeg";
+import palco1 from "./assets/palco1.png";
+import palco2 from "./assets/palco2.jpeg";
+import palco3 from "./assets/palco3.jpeg";
+import palco4 from "./assets/palco4.jpeg";
+import palco5 from "./assets/palco5.jpeg";
+import palco6 from "../home/assets/eventPhoto.png";
+import palco7 from "../home/assets/business.png";
+
+export const images = [
+  palco1,
+  palco2,
+  palco3,
+  palco4,
+  palco5,
+  palco6,
+  palco7,
+  eventImage,
+];
--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -5,13 +5,7 @@ import InitialBanner from "../../components/banner";
 
 //images
 import eventImage from "./assets/event-banner.jpeg";
-import palco1 from "./assets/palco1.png";
-import palco2 from "./assets/palco2.jpeg";
-import palco3 from "./assets/palco3.jpeg";
-import palco4 from "./assets/palco4.jpeg";
-import palco5 from "./assets/palco5.jpeg";
-import palco6 from "../home/assets/eventPhoto.png";
-import palco7 from "../home/assets/business.png";
+import { images } from "./images";
 
 const EventPage = () => {
   return (
@@ -29,14 +23,9 @@ const EventPage = () => {
 
       <Css.SectionsContainer>
         <Css.SectionContent>
-          <Css.Card src={eventImage} />
-          <Css.Card src={palco1} />
-          <Css.Card src={palco2} />
-          <Css.Card src={palco3} />
-          <Css.Card src={palco4} />
-          <Css.Card src={palco5} />
-          <Css.Card src={palco6} />
-          <Css.Card src={palco7} />
+          {images.map((image) => (
+            <Css.Card src={image} />
+          ))}
         </Css.SectionContent>
       </Css.SectionsContainer>
     </Css.EventContainer>
